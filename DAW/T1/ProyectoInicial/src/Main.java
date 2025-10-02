a4a365cc8d7cdae5f9d6a3275fa36a58f032123a
mmontes-vicente <m.montes.vicente@gmail.com>
2025-10-02 21:58:13 +0200

    update

--- DAW/T1/ProyectoInicial/src/Main.java
+++ DAW/T1/ProyectoInicial/src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+
+    //esto es un comentario
+    //otra linea de comentario
+    public static void main(String[] args){
+        System.out.println("Este es mi primer programa en Java");
+        System.out.println("Este es mi segunda linea del programa en Java");
+    }
+
+    public void nombreMetodo(){
+
+    }
 }
