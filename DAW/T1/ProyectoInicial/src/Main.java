93eac3bed93e09e809f9d846625344410affc8b1
mmontes-vicente <m.montes.vicente@gmail.com>
2025-10-05 18:33:46 +0200

    update

--- DAW/T1/ProyectoInicial/src/Main.java
+++ DAW/T1/ProyectoInicial/src/Main.java
@@ -1,13 +1,22 @@
 public class Main {
 
-    //esto es un comentario
-    //otra linea de comentario
-    public static void main(String[] args){
-        System.out.println("Este es mi primer programa en Java");
-        System.out.println("Este es mi segunda linea del programa en Java");
+    // esto es un comentario
+    /*
+    este comentario
+    se puede dividir en
+    varias lineas de codigo
+     */
+    // TODO me falta esto
+
+    public static void main(String[] args) {
+        System.out.println("Esta es la segunda linea del programa\n");
+        System.out.println("Este es mi primer programa en Java\n");
     }
 
-    public void nombreMetodo(){
+    public void nombreMetodo() {
 
     }
+
+    // TODO esto tambien esta pendiente
+
 }
