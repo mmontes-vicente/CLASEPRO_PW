42c3f75a97e5357dc560b8fc651eaa64855842af
mmontes-vicente <m.montes.vicente@gmail.com>
2025-10-10 02:48:38 +0200

    update

---
+++ DAW/Programacion/T1/Ejercicios/src/MainEjercicio1.java
@@ -0,0 +1,21 @@
+public class MainEjercicio1 {
+    /*
+    Ejercicio 1: Definir y mostrar variables
+    Crea un programa que defina tres variables: nombre, edad y ciudad. Asigna valores a cada una y muestra su contenido en la consola.
+    Ejemplo de salida por consola:
+    Ana
+    25
+    Madrid
+    */
+
+    public static void main(String[] args) {
+        String nombre= "Maria";
+        int edad= 25;
+        String ciudad= "Madrid";
+
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(ciudad);
+    }
+
+}
