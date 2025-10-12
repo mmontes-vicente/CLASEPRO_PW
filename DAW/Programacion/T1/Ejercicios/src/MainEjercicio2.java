5065478af332c2c721a06b86303f25268a8637da
mmontes-vicente <m.montes.vicente@gmail.com>
2025-10-12 04:29:47 +0200

    update

--- DAW/Programacion/T1/Ejercicios/src/MainEjercicio2.java
+++ DAW/Programacion/T1/Ejercicios/src/MainEjercicio2.java
@@ -1,6 +1,5 @@
 public class MainEjercicio2 {
-    /*
-    Ejercicio 2: Modificar variables
+    /*Ejercicio 2: Modificar variables
     Crea un programa que defina una variable llamada puntuación con valor inicial 0. Luego, modifica su valor tres veces y muestra el resultado final.
     Ejemplo de salida por consola:
     Puntuación inicial: 0
@@ -16,17 +15,17 @@ public class MainEjercicio2 {
         System.out.println("Puntuación inicial:" +puntuacion);
 
         // Primera modificación
-        puntuacion += +5;
+        puntuacion= 5;
 
         System.out.println("Después de primera modificación:" +puntuacion);
 
         // Segunda modificación
-        puntuacion = puntuacion + 5;
+        puntuacion = 5 + 5;
 
         System.out.println("Después de segunda modificación:" +puntuacion);
 
         // Puntuación final
-        puntuacion += 5;
+        puntuacion = 10 + 5;
 
         System.out.println("Puntuación final:" +puntuacion);
     }
