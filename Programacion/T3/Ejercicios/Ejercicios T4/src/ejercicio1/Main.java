91e0fef517af04d37ed1eec8c90aaf5c0625d9b2
mmontes-vicente <m.montes.vicente@gmail.com>
2025-12-23 17:00:46 +0100

    update

---
+++ Programacion/T3/Ejercicios/Ejercicios T4/src/ejercicio1/Main.java
@@ -0,0 +1,15 @@
+package ejercicio1;
+/*
+Crea un programa que permita sumar arrays multidimensionales. Para ello
+siguiente estos pasos:
+a. Pide al usuario el tamaño de las matrices
+b. Se crearán automáticamente dos matrices con números aleatorios entre 0 y
+50
+c. Se crea una tercera matriz llamada sumaMatrices donde se guarda la suma
+de las celdas de las generadas en el punto anterior
+d. Mostrar cada una de las matrices donde cada una tiene un titulo diferente
+para poder identificarlas
+*/
+public class Main {
+    public static void ,ain() {}
+}
